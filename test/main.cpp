129fbc6438b876624ab0c504b3af844c2845c166
Xxiong <xxiong@cqu.edu.cn>
2019-11-22 09:16:49 +0800

    fix: test_spk_setproctitle
    fix char** test_arch_test_argv bug

--- test/main.cpp
+++ test/main.cpp
@@ -2,12 +2,9 @@
 
 #include "test_arch/test_spk_setproctitle.hpp"
 
-int test_argc;
-char** test_argv;
-
 int main(int argc, char** argv) {
-    test_argc = argc;
-    test_argv = argv;
+    test_arch_test_argc = argc;
+    test_arch_test_argv = argv;
 
     testing::InitGoogleTest(&argc, argv);
     int err = RUN_ALL_TESTS();
--- test/test_arch/test_spk_setproctitle.hpp
+++ test/test_arch/test_spk_setproctitle.hpp
@@ -3,13 +3,16 @@
 
 #include <spk_setproctitle.h>
 
+int test_arch_test_argc;
+char** test_arch_test_argv;
+
 const int test_spk_setporoctitle_case_1(char** argv, const char* title) {
     int err = spk_setproctitle(argv, title);
     return err;
 }
 
 TEST(test_spk_setproctitle, case_1) {
-    ASSERT_EQ(test_spk_setporoctitle_case_1(test_argv, "sparkle_test"), 0);
+    ASSERT_EQ(test_spk_setporoctitle_case_1(test_arch_test_argv, "sparkle_test"), 0);
 }
 
 #endif // SPARKLE_TEST_SPK_SET_PROCTITLE_H_
