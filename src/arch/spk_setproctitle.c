05f06a27ab50061ba6a07ede8168a73f8a854f16
Xxiong <xxiong@cqu.edu.cn>
2019-11-22 09:30:23 +0800

    refactor: append spk_setproctitle() to support new feature
    append spk_setproctitle(), while new title size < sizeof(argv[][]), then
    need not move **environ to new area.

--- src/arch/spk_setproctitle.c
+++ src/arch/spk_setproctitle.c
@@ -9,30 +9,45 @@ extern char** environ;
 
 int spk_setproctitle(char** argv, const char* title) {
     char* last = NULL, *p = NULL;
-    size_t size = 0;
+    size_t size, title_size;
     int i;
 
-    for (i = 0; environ[i]; i++) {
-        size += strlen(environ[i]) + 1;
-    }
-
-    p = (char*)malloc(size);
-    if (!p) {
-        return -1;
-    }
-
-    last = argv[0];
+    title_size = strlen(title) + 1;
+    size = 0;
     for (i = 0; argv[i]; i++) {
-        last += strlen(argv[i]) + 1;
+        size += strlen(argv[i]) + 1;
     }
 
-    for (i = 0; environ[i]; i++) {
-        size = strlen(environ[i]) + 1;
-        last += size;
+    if (size >= title_size) {
+        last = argv[0];
+        for (i = 0; argv[i]; i++) {
+            last += strlen(argv[i]) + 1;
+        }
+    }
+    else {  // move char* environ to char* p
+        size = 0;
+        for (i = 0; environ[i]; i++) {
+            size += strlen(environ[i]) + 1;
+        }
 
-        strncpy(p, environ[i], size);
-        environ[i] = p;
-        p += size;
+        p = (char*)malloc(size);
+        if (!p) {
+            return -1;
+        }
+
+        last = argv[0];
+        for (i = 0; argv[i]; i++) {
+            last += strlen(argv[i]) + 1;
+        }
+
+        for (i = 0; environ[i]; i++) {
+            size = strlen(environ[i]) + 1;
+            last += size;
+
+            strncpy(p, environ[i], size);
+            environ[i] = p;
+            p += size;
+        }
     }
 
     last--;
